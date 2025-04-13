f9cc11bfa3c06be3f1bbe273a4ecd44e79c7a834
Arne Goedeke <arne@deuso.de>
2025-04-13 11:47:09 +0200

    mdns-browse: Add some test cases

--- mdns-browse/test/local.test.js
+++ mdns-browse/test/local.test.js
@@ -2,12 +2,55 @@ import { afterEach, describe, test } from "node:test";
 import { equal } from "node:assert/strict";
 import { find } from "mdns-browse";
 import { announceService } from "simple-mdns-announce";
+import { randomBytes } from "node:crypto";
+import { deepEqual } from "node:assert";
+
+async function findService(service) {
+  const [type, protocol] = service.type.split(".");
+  const result = await find(
+    {
+      type,
+      protocol,
+    },
+    (result) => {
+      return result.name === service.name;
+    },
+    AbortSignal.timeout(60 * 1000)
+  );
+
+  equal(result.type, type);
+  equal(result.protocol, protocol);
+  equal(result.port, service.port);
+
+  if (service.txtRecords) {
+    const tmp = Object.entries(service.txtRecords).map(([key, value]) => {
+      return { key, value };
+    });
+    deepEqual(tmp, result.txtRecords || []);
+  }
+}
+
+function randomizeServiceName(service) {
+  return {
+    ...service,
+    name: "test-" + randomBytes(8).toString("hex"),
+    port: 1 + Math.round(Math.random() * 0xfff1),
+  };
+}
+
+const type = "_ttest._tcp";
+
+const exampleServices = {
+  basic: { type },
+  "with txtRecord": { type, txtRecords: { foo: "bar" } },
+  "with txtRecords": { type, txtRecords: { foo: "bar", bar: "foo" } },
+  "with empty txtRecord": { type, txtRecords: { foo: "bar", bar: "" } },
+};
 
 describe("local", () => {
   let cleanup;
 
   afterEach(() => {
-    console.log("after each");
     if (!cleanup) return;
     try {
       cleanup();
@@ -16,27 +59,28 @@ describe("local", () => {
     }
   });
 
-  test("simple find", { concurrency: false }, async () => {
-    const service = {
-      name: "testing",
-      type: "_ttest._tcp",
-      port: 1234,
-    };
-    cleanup = announceService(service);
-
-    const result = await find(
-      {
-        type: "_ttest",
-        protocol: "_tcp",
-      },
-      (result) => {
-        return result.name === service.name;
-      },
-      AbortSignal.timeout(30 * 1000)
+  for (const [variant, srv] of Object.entries(exampleServices)) {
+    test(
+      `simple find after announce (${variant})`,
+      { concurrency: true },
+      async () => {
+        const service = randomizeServiceName(srv);
+        cleanup = announceService(service);
+        await new Promise((resolve) => setTimeout(resolve, 4000));
+        await findService(service);
+      }
     );
 
-    equal(result.name, service.name);
-    equal(result.port, service.port);
-    cleanup();
-  });
+    test(
+      `simple find before announce (${variant})`,
+      { concurrency: true },
+      async () => {
+        const service = randomizeServiceName(srv);
+        const findPromise = await findService(service);
+        await new Promise((resolve) => setTimeout(resolve, 4000));
+        cleanup = announceService(service);
+        await findPromise;
+      }
+    );
+  }
 });
